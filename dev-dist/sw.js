25f06db899a93aa573f099e8970334232e9babe4
Alissonn1325 <alissonfbrito01@gmail.com>
2024-10-29 14:34:30 -0300

    Alterações no Footer

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.k0gkb9ep7cg"
+    "revision": "0.h5cmd2ksu"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
